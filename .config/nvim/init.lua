dd3d74295b01eb8c66963c82c08229223bb3c12f
Asem Ashraf <asem.ashraf18@gmail.com>
2023-02-28 01:22:14 +0200

    Updated nvim, picom and polybar

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,6 +7,13 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   vim.cmd [[packadd packer.nvim]]
 end
 
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
 require('packer').startup(function(use)
   -- Package manager
   use 'wbthomason/packer.nvim'
@@ -56,14 +63,23 @@ require('packer').startup(function(use)
   use 'tpope/vim-rhubarb'
   use 'lewis6991/gitsigns.nvim'
   use 'mbbill/undotree'
+  use 'github/copilot.vim'
+
 
   use 'navarasu/onedark.nvim' -- Theme inspired by Atom
   use 'nvim-lualine/lualine.nvim' -- Fancier statusline
   use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
   use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
+  use {
+    'kylechui/nvim-surround', -- "cs"' to change surrounding '' to ""
+    config = function()
+      require('nvim-surround').setup ({
 
-  -- Fuzzy Finder (files, lsp, etc)
+      })
+    end
+  }
+
   use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
 
   -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
@@ -149,6 +165,7 @@ vim.g.maplocalleader = ' '
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', '<F5>',":UndotreeToggle<CR>" ,{ silent = true })
+vim.keymap.set('n', 'tt',":NvimTreeToggle<CR>" ,{ silent = true })
 
 -- Keymap to easily switch between open windows in one tab.
 vim.keymap.set('n', ' ',"<C-w>" ,{})
@@ -180,6 +197,9 @@ require('lualine').setup {
     section_separators = '',
   },
 }
+
+-- empty setup using defaults
+require("nvim-tree").setup()
 
 -- Enable Comment.nvim
 require('Comment').setup()
@@ -420,7 +440,7 @@ cmp.setup {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-    ['<Tab>'] = cmp.mapping(function(fallback)
+    ['<Alt-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
@@ -429,7 +449,7 @@ cmp.setup {
         fallback()
       end
     end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
+    ['<Alt-S-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
