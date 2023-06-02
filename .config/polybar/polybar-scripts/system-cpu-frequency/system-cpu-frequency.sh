eb10cb9860fffb066653f554ad05a44fe8aaf465
Asem Ashraf <asem.ashraf18@gmail.com>
2023-06-02 17:02:47 +0300

    Fixed the frequency script to be more accurate

--- .config/polybar/polybar-scripts/system-cpu-frequency/system-cpu-frequency.sh
+++ .config/polybar/polybar-scripts/system-cpu-frequency/system-cpu-frequency.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
-c=0;t=0
-
-awk '/MHz/ {print $4}' < /proc/cpuinfo | (while read -r i
-do
-    t=$( echo "$t + $i" | bc )
-    c=$((c+1))
+t=0
+for ((k=0; k<16; k++)); do
+    freq_file="/sys/devices/system/cpu/cpu$k/cpufreq/scaling_cur_freq"
+    if [ -f "$freq_file" ]; then
+        # Read the frequency value from the file
+        i=$(awk '{printf "%.0f\n", $1/1000}' "$freq_file")
+        if [ -n "$i" ]; then
+            t=$( echo "$t + $i" | bc )
+        fi
+    fi
 done
-echo "scale=2; $t / $c / 1000" | bc | awk '{print $1"GHz"}')
+
+if [ "$t" -eq 0 ]; then
+    echo "No CPU frequency values found in any of the files."
+    exit 1
+fi
+
+echo "scale=2; $t / 16 / 1000" | bc | awk '{print $1"GHz"}'
